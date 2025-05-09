8b764e5892ddeae518c1165ceac7857d4462e4c0
Danya <dselivanov143@mail.ru>
2025-05-09 04:51:29 +0300

    Initial

---
+++ backend/src/router/ideas/createIdea/input.ts
@@ -0,0 +1,10 @@
+import { zNickRequired, zStringMin, zStringRequired } from '@ideanick/shared/src/zod'
+import { z } from 'zod'
+
+export const zCreateIdeaTrpcInput = z.object({
+  name: zStringRequired,
+  nick: zNickRequired,
+  description: zStringRequired,
+  text: zStringMin(100),
+  images: z.array(zStringRequired),
+})
---
+++ webapp/src/lib/env.ts
@@ -0,0 +1,16 @@
+import { zEnvHost, zEnvNonemptyTrimmed, zEnvNonemptyTrimmedRequiredOnNotLocal } from '@ideanick/shared/src/zod'
+import { z } from 'zod'
+
+export const zEnv = z.object({
+  NODE_ENV: z.enum(['development', 'production']),
+  HOST_ENV: zEnvHost,
+  SOURCE_VERSION: zEnvNonemptyTrimmedRequiredOnNotLocal,
+  VITE_BACKEND_TRPC_URL: zEnvNonemptyTrimmed,
+  VITE_WEBAPP_URL: zEnvNonemptyTrimmed,
+  VITE_WEBAPP_SENTRY_DSN: zEnvNonemptyTrimmedRequiredOnNotLocal,
+  VITE_CLOUDINARY_CLOUD_NAME: zEnvNonemptyTrimmed,
+})
+
+const envFromBackend = (window as any).webappEnvFromBackend
+// eslint-disable-next-line node/no-process-env
+export const env = zEnv.parse(envFromBackend?.replaceMeWithPublicEnv ? process.env : envFromBackend)
